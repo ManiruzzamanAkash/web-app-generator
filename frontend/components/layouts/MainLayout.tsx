d3610983f78338cec163f2c5667386701be4c98b
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2021-10-29 09:22:34 +0600

    Added tailwind css

--- frontend/components/layouts/MainLayout.tsx
+++ frontend/components/layouts/MainLayout.tsx
@@ -13,7 +13,20 @@ const MainLayout: FunctionComponent<Props> = () => {
             <meta name="description" content="Web App Generator" />
             <link rel="icon" href="/favicon.ico" />
             <Header />
-            <p>Body Part</p>
+
+            {/* This would remove, just added to test tailwind css */}
+            <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+                {/* <div className="flex-shrink-0">
+                    <img className="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo" />
+                </div> */}
+                <div>
+                    <div className="text-xl font-medium text-black">Demo Card using Tailwind CSS</div>
+                    <p className="text-gray-500">
+                        A simple next js card example...
+                    </p>
+                </div>
+            </div>
+
             <Footer />
         </div>
     )
